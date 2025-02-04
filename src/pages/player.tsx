2c1c0f8e94bc75986f919b022b308af013e6c7c4
ilSPRITEli <gvkgodvilekung@gmail.com>
2025-02-05 06:23:44 +0700

    SOR3K-13: mock not scan

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -1,11 +1,14 @@
 'use client';
-import { Session } from 'next-auth';
+
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { ChevronLeft, UserRoundCheck } from 'lucide-react';
 
+import { useSession } from 'next-auth/react';
+
 import PlayerCard from './playersection/playercard';
 import PlayerTable from './playersection/playertable';
+import PlayerNotConnect from './playersection/playetNotConnect';
 
 import { Player, Matches } from '@/app/players/[id]/page';
 
@@ -15,8 +18,13 @@ type PlayerProfileProps = Readonly<{
   matchesData: Matches[];
 }>;
 
+const isRegistered = false;
+
 const PlayerPage = ({ playerID, playerData, matchesData }: PlayerProfileProps) => {
+  const { data: session } = useSession();
+
   return (
+
     <div className="min-h-screen w-full bg-gradient-to-b from-white to-[#FFC72C]/10 px-5 py-5 flex flex-col gap-10">
       <div className="w-full flex flex-row justify-between items-center">
         <Link
@@ -27,22 +35,44 @@ const PlayerPage = ({ playerID, playerData, matchesData }: PlayerProfileProps) =
           <ChevronLeft />
           กลับหน้าแรก{' '}
         </Link>
-        <Link
-          href="/"
-          className="flex flex-row w-fit gap-1 rounded-full bg-firsto border-1 border-firsto  px-5 py-2 text-secondw"
-        >
-          {' '}
-          ลงทะเบียน <UserRoundCheck />{' '}
-        </Link>
+        {session?.user?.email?.split('@')[0] === playerData?.studentID ? (
+          <Link
+            href="/"
+            className="flex flex-row w-fit gap-1 rounded-full bg-firsto border-1 border-firsto  px-5 py-2 text-secondw"
+          >
+            {' '}
+            ลงทะเบียนแข่ง <UserRoundCheck />{' '}
+          </Link>
+        ) : (
+          <></>
+        )}
+
       </div>
 
       <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }}>
         <div className="flex xl:flex-row flex-col justify-center gap-5 items-center lg:items-start content-start ">
-          <PlayerCard playerData={playerData} />
-          <PlayerTable matchesData={matchesData} />
+          {session ? (
+            <>
+              {session?.user?.email?.split('@')[0] === playerData?.studentID && isRegistered ? (
+                <>
+                  <PlayerCard playerData={playerData} />
+                  <PlayerTable matchesData={matchesData} />
+                </>
+              ) : (
+                <>
+                  <PlayerNotConnect />
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <PlayerCard playerData={playerData} />
+              <PlayerTable matchesData={matchesData} />
+            </>
+          )}
         </div>
-      </motion.div>
-    </div>
+      </motion.div >
+    </div >
   );
 };
 
