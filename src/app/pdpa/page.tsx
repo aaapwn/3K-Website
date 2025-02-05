e548c022a255c8dbf9b06b0322b9509fcdd2814d
Siwakorn Somjit <65070020@kmitl.ac.th>
2025-02-05 17:13:40 +0700

    SOR3K-13:PDPA update

--- src/app/pdpa/page.tsx
+++ src/app/pdpa/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Page = async () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen pb-10">
       <iframe
         src="https://drive.google.com/file/d/1FwO68ke7lxAAiKZwrBlhdmcdXsHiRLO5/preview"
-        className="w-full h-screen"
+        className="w-full h-full absolute"
         allow="autoplay"
       ></iframe>
     </div>
