2b447635d48273e448c1016f199622273492a715
Siwakorn Somjit <65070020@kmitl.ac.th>
2025-02-07 15:12:02 +0700

    test

--- src/pages/landingsection/sportlist.tsx
+++ src/pages/landingsection/sportlist.tsx
@@ -1,49 +1,66 @@
-"use client";
+'use client';
 
-import { Button } from "@heroui/button";
-import SportBadge from "@/components/sport/sportbadge";
+import { Button } from '@heroui/button';
+import SportBadge from '@/components/sport/sportbadge';
 
 type Sport = {
-    en: string;
-    th: string;
-}
+  en: string;
+  th: string;
+};
 
 const sports: Sport[] = [
-    { en: 'football', th: 'ฟุตบอล' },
-    { en: 'petong', th: 'ฟุตซอล' },
-    { en: 'basketball', th: 'บาสเกตบอล' },
-    { en: 'volleyball', th: 'วอลเลย์บอล' },
-    { en: 'badminton', th: 'แบดมินตัน' },
-    { en: 'tableTennis', th: 'เทเบิลเทนนิส' },
-    { en: 'running', th: 'กรีฑา' },
-    { en: 'bowling', th: 'เปตอง' },
-    { en: 'chess', th: 'หมากกระดาน' },
-    { en: 'gaming', th: 'อีสปอร์ต' },
-]
+  { en: 'football', th: 'ฟุตบอล' },
+  { en: 'petong', th: 'ฟุตซอล' },
+  { en: 'basketball', th: 'บาสเกตบอล' },
+  { en: 'volleyball', th: 'วอลเลย์บอล' },
+  { en: 'badminton', th: 'แบดมินตัน' },
+  { en: 'tableTennis', th: 'เทเบิลเทนนิส' },
+  { en: 'running', th: 'กรีฑา' },
+  { en: 'petong', th: 'เปตอง' },
+  { en: 'chess', th: 'หมากกระดาน' },
+  { en: 'gaming', th: 'อีสปอร์ต' },
+];
 
 const SportSection = () => {
-    return (
-        <div className="md:py-24 py-10 md:px-32 px-5 flex flex-col justify-between items-center bg-secondy/10 md:gap-10 gap-5">
-            <h1 className="font-bold md:text-6xl text-4xl leading-[64px]">รายการกีฬา</h1>
-            <h2 className="text-center md:text-xl text-lg">เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ <br />มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16 </h2>
-            <div className="md:flex md:gap-10 gap-2 md:flex-row flex-col w-full md:w-auto hidden">
-                <Button onPress={() => null} className='bg-firsto text-secondw rounded-md text-medium px-16'>กีฬาทั้งหมด</Button>
-                <Button onPress={() => null} className='bg-secondw text-tertbg rounded-md text-medium border-tertbg border-1 px-16'>กำหนดการ</Button>
-            </div>
-            <div className="grid grid-cols-2 gap-10 md:grid-cols-3 xl:grid-cols-5">
-                {/* <div className="flex justify-center items-center md:gap-x-[80px] md:gap-y-10 align-middle flex-wrap md:px-20 gap-x-10"> */}
-                {
-                    sports.map((sport, index) => (
-                        <SportBadge key={index} sport={sport.en} className="md:scale-100 scale-85">{sport.th}</SportBadge>
-                    ))
-                }
-            </div>
-            <div className="md:hidden md:gap-10 gap-2 md:flex-row flex-col w-full md:w-auto flex">
-                <Button onPress={() => null} className='bg-firsto text-secondw rounded-md text-medium px-16'>กีฬาทั้งหมด</Button>
-                <Button onPress={() => null} className='bg-secondw text-tertbg rounded-md text-medium border-tertbg border-1 px-16'>กำหนดการ</Button>
-            </div>
-        </div >
-    );
-}
+  return (
+    <div className="md:py-24 py-10 md:px-32 px-5 flex flex-col justify-between items-center bg-secondy/10 md:gap-10 gap-5">
+      <h1 className="font-bold md:text-6xl text-4xl leading-[64px]">รายการกีฬา</h1>
+      <h2 className="text-center md:text-xl text-lg">
+        เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ <br />
+        มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16{' '}
+      </h2>
+      <div className="md:flex md:gap-10 gap-2 md:flex-row flex-col w-full md:w-auto hidden">
+        <Button onPress={() => null} className="bg-firsto text-secondw rounded-md text-medium px-16">
+          กีฬาทั้งหมด
+        </Button>
+        <Button
+          onPress={() => null}
+          className="bg-secondw text-tertbg rounded-md text-medium border-tertbg border-1 px-16"
+        >
+          กำหนดการ
+        </Button>
+      </div>
+      <div className="grid grid-cols-2 gap-10 md:grid-cols-3 xl:grid-cols-5">
+        {/* <div className="flex justify-center items-center md:gap-x-[80px] md:gap-y-10 align-middle flex-wrap md:px-20 gap-x-10"> */}
+        {sports.map((sport, index) => (
+          <SportBadge key={index} sport={sport.en} className="md:scale-100 scale-85" padding="p-10">
+            {sport.th}
+          </SportBadge>
+        ))}
+      </div>
+      <div className="md:hidden md:gap-10 gap-2 md:flex-row flex-col w-full md:w-auto flex">
+        <Button onPress={() => null} className="bg-firsto text-secondw rounded-md text-medium px-16">
+          กีฬาทั้งหมด
+        </Button>
+        <Button
+          onPress={() => null}
+          className="bg-secondw text-tertbg rounded-md text-medium border-tertbg border-1 px-16"
+        >
+          กำหนดการ
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default SportSection;
--- src/pages/sportPage.tsx
+++ src/pages/sportPage.tsx
@@ -1,45 +1,44 @@
 'use client';
 
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Chip } from "@heroui/react";
-import SportBadge from "@/components/sport/sportbadge";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Chip } from '@heroui/react';
+import SportBadge from '@/components/sport/sportbadge';
 
 type sportPageProps = {
-    sportData: {
-        en: string;
-        th: string;
-        type: string[];
-    }[];
+  sportData: {
+    en: string;
+    th: string;
+    type: string[];
+  }[];
 };
 
 export default function SportPage({ sportData }: sportPageProps) {
-    return (
-        <div className="flex flex-col justify-center lg:px-32 md:px-14 px-10 py-10">
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-                {
-                    sportData.map((sport, index) => (
-                        <Card className="" key={sport.en}>
-                            <CardHeader className="flex gap-1 bg-secondy/10 p-5 items-center">
-                                <SportBadge sport={sport.en} className="scale-50" key={sport.en}></SportBadge>
-                                <p className="text-3xl font-bold">{sport.th}</p>
-                            </CardHeader>
-                            <Divider />
-                            <CardBody>
-                                <p>เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์ มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16 </p>
-                            </CardBody>
-                            <Divider />
-                            <CardFooter className="flex gap-2 w-full flex-wrap">
-
-                                {sport.type.map((type, index) => (
-                                    <Chip key={sport.en + index}>
-                                        {type}
-                                    </Chip>
-                                ))}
-
-                            </CardFooter>
-                        </Card>
-                    ))
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center lg:px-32 md:px-14 px-10 py-10">
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        {sportData.map((sport, index) => (
+          <Card className="" key={sport.en}>
+            <CardHeader className="flex gap-1 bg-secondy/10 p-5 items-center">
+              <SportBadge sport={sport.en} className="scale-50" key={sport.en} padding="p-2"></SportBadge>
+              <p className="text-4xl font-bold">{sport.th}</p>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <p>
+                เนื้อหาจำลองแบบเรียบๆ ที่ใช้กันในธุรกิจงานพิมพ์หรืองานเรียงพิมพ์
+                มันได้กลายมาเป็นเนื้อหาจำลองมาตรฐานของธุรกิจดังกล่าวมาตั้งแต่ศตวรรษที่ 16{' '}
+              </p>
+            </CardBody>
+            <Divider />
+            <CardFooter className="flex gap-2 w-full flex-wrap">
+              {sport.type.map((type, index) => (
+                <Chip key={sport.en + index} className="text-xl py-2">
+                  {type}
+                </Chip>
+              ))}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
