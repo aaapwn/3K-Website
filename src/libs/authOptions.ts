28483b22a0afe7b4feb98015c868e657ea96929b
aaapwn <armmykung2547@gmail.com>
2025-02-21 20:43:17 +0700

    Add sessionToken cookie configuration in authOptions

--- src/libs/authOptions.ts
+++ src/libs/authOptions.ts
@@ -53,4 +53,15 @@ export const authOptions: AuthOptions = {
   },
   debug: true,
   useSecureCookies: process.env.NODE_ENV === "production",
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        path: "/",
+      },
+    },
+  },
 };
