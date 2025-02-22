aa8110bf09ff747a9c0a679c49c0281778bc71e9
aaapwn <armmykung2547@gmail.com>
2025-02-22 13:20:07 +0700

    refactor: comment out unused auth import in add-medal page for cleaner code

--- src/app/admin/add-medal/page.tsx
+++ src/app/admin/add-medal/page.tsx
@@ -1,7 +1,7 @@
 'use server';
 
 import MedalForm from '@/views/add-medal-page.';
-import auth from '@/libs/auth';
+// import auth from '@/libs/auth';
 
 export default async function AdminAddResult() {
   // const session = await auth();
